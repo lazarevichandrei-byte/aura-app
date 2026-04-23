55dc95d6531890c8e598a99ca17427e507159133
Andrei <lazarevichandrei@gmail.com>
2026-04-24 01:10:58 +0300

    fix UI: gallery grid + buttons + interests

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,13 @@
+"use client";
+
+import BottomNav from "../../components/BottomNav";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div style={{ paddingBottom: "80px" }}>
+      <h1>Home</h1>
+
+      <BottomNav />
+    </div>
+  );
 }
