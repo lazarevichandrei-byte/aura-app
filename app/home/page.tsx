a0cfaebd757d4547adaf39a4dd949454a09a6c5c
Andrei <lazarevichandrei@gmail.com>
2026-04-24 14:50:24 +0300

    fix UI: gallery grid + buttons + interests

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -326,7 +326,7 @@ position:"absolute",
 left:0,
 right:0,
 bottom:0,          // начинается С САМОГО НИЗА фото
-height:"%",      // доходит до надписей
+height:"45%",      // доходит до надписей
 zIndex:5,
 pointerEvents:"none",
 background:`
